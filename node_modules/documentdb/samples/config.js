133aa4b75442ad6879cc58765b64c365ecc12223
Jay Bienvenu <github@bienv.com>
2015-05-09 19:45:33 -0500

    Delete unneeded sample and tutorial files.

--- node_modules/documentdb/samples/config.js
+++
@@ -1,9 +0,0 @@
-﻿exports.connection = {
-    endpoint: 'https://ryancrawcour.documents.azure.com:443/',
-    authKey: 'NC4KZ6Lh5/wukMHMunWlFx6qWfjwMV1MIODmu4WfvL9smr1aT05wT2nD/w4+CKY49ljXHmNK6x8+ejMK3k64SQ=='
-};
-
-exports.names = {
-	database: 'NodeSamples',
-	collection: 'Families',
-};
